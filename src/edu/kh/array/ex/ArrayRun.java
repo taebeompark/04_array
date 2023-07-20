30b6216e61f2310b8f3fd77901c0b40aa9d843b3
taebeom <ptbyahoo12@gmail.com>
2023-07-20 16:32:09 +0900

    ptb commit

--- src/edu/kh/array/ex/ArrayRun.java
+++ src/edu/kh/array/ex/ArrayRun.java
@@ -15,9 +15,9 @@ public class ArrayRun {
 		//arrayEx1.ex9();
 		ArrayExample2 arrayEx2 = new ArrayExample2();
 		//arrayEx2.shallowCopy();
-		//arrayEx2.shallowCopy1();
 		//arrayEx2.deepCopy();
 		arrayEx2.createLottoNumber();
+		
 		
 	}
 
