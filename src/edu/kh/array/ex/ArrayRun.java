64e012bb9684f1632ce2df67f8c784ae37467e2d
taebeompark <ptbyahoo12@gmail.com>
2023-07-26 17:20:41 +0900

    완료

--- src/edu/kh/array/ex/ArrayRun.java
+++ src/edu/kh/array/ex/ArrayRun.java
@@ -7,7 +7,7 @@ public class ArrayRun {
 		//arrayEx1.ex1();
 		//arrayEx1.ex2();
 		//arrayEx1.ex3();
-		//arrayEx1.ex4();
+		arrayEx1.ex4();
 		//arrayEx1.ex5();
 		//arrayEx1.ex6();
 		//arrayEx1.ex7();
@@ -16,7 +16,7 @@ public class ArrayRun {
 		ArrayExample2 arrayEx2 = new ArrayExample2();
 		//arrayEx2.shallowCopy();
 		//arrayEx2.deepCopy();
-		arrayEx2.createLottoNumber();
+		//arrayEx2.createLottoNumber();
 		
 		
 	}
--- src/edu/kh/array/practice/run/PracticeRun/PracticeRun.java
+++ src/edu/kh/array/practice/run/PracticeRun/PracticeRun.java
@@ -7,9 +7,9 @@ public class PracticeRun {
 		op.practice1();
 		//op.practice2();
 		//op.practice3();
-		op.practice4();
-		op.practice5();
-		op.practice6();
-		op.practice7();
+		//op.practice4();
+		//op.practice5();
+		//op.practice6();
+		//op.practice7();
 	}
 }
